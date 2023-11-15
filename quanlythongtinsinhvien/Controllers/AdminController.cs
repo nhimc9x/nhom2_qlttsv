e0251859037a5627aa07075ec97fca36d8705a9e
nhimc9x <nhicomputer77@gmail.com>
2023-11-15 12:19:44 +0700

    Add project files.

---
+++ quanlythongtinsinhvien/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using quanlythongtinsinhvien.Models;
+using System.Diagnostics;
+
+namespace quanlythongtinsinhvien.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ILogger<AdminController> _logger;
+
+        public AdminController(ILogger<AdminController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ quanlythongtinsinhvien/Controllers/ContentController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using quanlythongtinsinhvien.Models;
+using System.Diagnostics;
+
+namespace quanlythongtinsinhvien.Controllers
+{
+    public class ContentController : Controller
+    {
+        private readonly ILogger<ContentController> _logger;
+
+        public ContentController(ILogger<ContentController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
