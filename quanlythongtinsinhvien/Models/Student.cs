b3aa66793d905b5797a99f8a6f20bb41efd2e5f8
nhimc9x <nhicomputer77@gmail.com>
2023-11-15 15:06:41 +0700

    Code Day #2

---
+++ quanlythongtinsinhvien/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace quanlythongtinsinhvien.Models;
+
+public partial class Student
+{
+    public string Masv { get; set; } = null!;
+
+    public string Tensv { get; set; } = null!;
+
+    public string? Ngaysinh { get; set; }
+
+    public string? Quequan { get; set; }
+
+    public string? Sodienthoai { get; set; }
+
+    public string? Ghichu { get; set; }
+
+    public string? Gioitinh { get; set; }
+
+    public int Id { get; set; }
+}
